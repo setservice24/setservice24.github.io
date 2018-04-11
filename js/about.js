70c5254f9a139b3621e33ef2a5d19cea17c34a75
Evgeny Babkin <babkinevgeny@gmail.com>
2018-04-11 14:54:53 +0300

    sdf

--- js/about.js
+++
@@ -1,7 +0,0 @@
-$('.questions-title .container').click(function() {
-  if($(this).parent().siblings('.container').children('.questions-item-content').hasClass('questions-item-content-show')) {
-    $(this).parent().siblings('.container').children('.questions-item-content').slideUp().removeClass('questions-item-content-show');
-  } else {
-    $(this).parent().siblings('.container').children('.questions-item-content').slideDown().addClass('questions-item-content-show');
-  }
-});
